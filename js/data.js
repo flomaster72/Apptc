0c179a8e767becc1d7bb38427698d68ee24396b1
flomaster72 <sirpranav125@gmail.com>
2026-05-09 20:39:36 +0530

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,15 @@
+const tcData = {
+    validCredentials: { username: "User123", password: "User123" },
+    locations: [
+        "CentralHub_L_MUMX",
+        "CentralHub_L_DEL8",
+        "SatelliteHub_SRO",
+        "CentralHub_L_KAL1",
+        "CentralHub_FPT"
+    ],
+    shipments: [
+        { id: "FMPP3977414022", weight: "1.00KG", status: "Delivered" },
+        { id: "LUAC0001067694", weight: "7.50KG", status: "Delivered" },
+        { id: "FMPP1234567890", weight: "2.30KG", status: "Expected" }
+    ]
+};
